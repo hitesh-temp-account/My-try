6bbc4779d22fd20a6c87b6906adb0fb59692650a
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2023-12-28 12:43:49 +0530

    Create RandomUnitTestClass.java

---
+++ app/src/test/java/com/example/breach/RandomUnitTestClass.java
@@ -0,0 +1,15 @@
+package com.example.breach;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class RandomUnitTestClass {
+
+    @Test
+    public void verifyUtilMethod2() {
+        int result = new TempFeatureUtils().utilMethod2();
+        assertThat(result, is(30));
+    }
+}
