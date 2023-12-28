010494d221092d6d1af509e44abc27858fc1aa81
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2023-12-28 12:41:42 +0530

    Create TempFeatureUtils.java

---
+++ app/src/main/java/com/example/breach/TempFeatureUtils.java
@@ -0,0 +1,26 @@
+package com.example.quickcart;
+
+/**
+ * This unused class is intentionally added to check
+ * its removal after enabling ProGuard
+ */
+public class TempFeatureUtils {
+
+    public void utilMethod1() {
+        // Just some random logic
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+    }
+
+    public int utilMethod2() {
+        // Just some random logic
+        int a = 10;
+        int b = 20;
+        return a + b;
+    }
+
+    public void utilMethod3() {
+        // do nothing
+    }
+}
