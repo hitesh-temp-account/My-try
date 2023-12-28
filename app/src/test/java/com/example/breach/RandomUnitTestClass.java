c87353952bd3a277cc8b53034b38309c951f3e7e
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2023-12-28 12:56:39 +0530

    Update RandomUnitTestClass.java

--- app/src/test/java/com/example/breach/RandomUnitTestClass.java
+++ app/src/test/java/com/example/breach/RandomUnitTestClass.java
@@ -8,6 +8,12 @@ import org.junit.Test;
 public class RandomUnitTestClass {
 
     @Test
+    public void verifyUtilMethod1() {
+        int result = new TempFeatureUtils().utilMethod1();
+        assertThat(result, is(30));
+    }
+    
+    @Test
     public void verifyUtilMethod2() {
         int result = new TempFeatureUtils().utilMethod2();
         assertThat(result, is(30));
