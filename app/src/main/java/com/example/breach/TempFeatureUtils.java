9dd6dda5eef47441878ec8f3b65a8dfe7489a355
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2023-12-28 13:13:31 +0530

    Added new file and test (#268)
    * Create TempFeatureUtils.java
    
    * Create RandomUnitTestClass.java
    
    * Update TempFeatureUtils.java
    
    * Update RandomUnitTestClass.java
    
    * Update TempFeatureUtils.java

---
+++ app/src/main/java/com/example/breach/TempFeatureUtils.java
@@ -0,0 +1,26 @@
+package com.example.breach;
+
+/**
+ * This unused class is intentionally added to check
+ * its removal after enabling ProGuard
+ */
+public class TempFeatureUtils {
+
+    public int utilMethod1() {
+        // Just some random logic
+        int a = 10;
+        int b = 20;
+        return a + b;
+    }
+
+    public int utilMethod2() {
+        // Just some random logic
+        int a = 10;
+        int b = 20;
+        return a + b;
+    }
+
+    public void utilMethod3() {
+        // do nothing
+    }
+}
