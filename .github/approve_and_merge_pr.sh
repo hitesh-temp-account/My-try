bf953d995300a87c396669bddf439d6d121d4a7c
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2024-01-02 11:49:29 +0530

    Update approve_and_merge_pr.sh

--- .github/approve_and_merge_pr.sh
+++ .github/approve_and_merge_pr.sh
@@ -1,23 +1,24 @@
+#!/bin/bash
+
 # take input
 pr_number=$1
 app_token=$2
 github_token=$3
 
-
+# Approve PR
 - name: Approve PR
-  if: pr_number
+  if: $pr_number
   run: |
-  gh pr review pr_number --approve
-  echo "approved"
+    gh pr review $pr_number --approve
+    echo "approved"
   env:
-    GITHUB_TOKEN: app_token
+    GITHUB_TOKEN: $app_token
 
+# Turn On PR Auto-Merge
 - name: Turn On PR Auto-Merge
   id: auto-merge
-  if: pr_number
-  run: >
-    gh pr merge --squash --auto 
-    --repo ${{ github.repository }} --delete-branch
-    pr_number
+  if: $pr_number
+  run: |
+    gh pr merge --squash --auto --repo ${{ github.repository }} --delete-branch $pr_number
   env:
-    GITHUB_TOKEN: github_token
+    GITHUB_TOKEN: $github_token
