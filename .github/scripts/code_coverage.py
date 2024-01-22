f98d7d156205b2e3f79bf4ad66af02102a8cc348
bindhu-22 <94899531+bindhu-22@users.noreply.github.com>
2024-01-22 17:56:43 +0530

    Update code_coverage.py

--- .github/scripts/code_coverage.py
+++ .github/scripts/code_coverage.py
@@ -18,7 +18,7 @@ def code_coverage_report():
     html += f"<tr><td>{start_tag}{link_tag}{table_list[index]}{end_tag}</td>" \
             f"<td>{start_tag}<img src={format_percentage(table_list[index+1])}>{end_tag}</td></tr>"
     html += "</table><br><i>MIN Cverage: 35%</i>"
-    print(html)
+  print(html)
 
 def get_table_list(file_paths):
   coveredSum=0
