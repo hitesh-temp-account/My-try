f93bbf659317d9b8a1c5aa26c800571aac493af3
bindhu-22 <94899531+bindhu-22@users.noreply.github.com>
2024-01-22 18:28:28 +0530

    Update code_coverage.py

--- .github/scripts/code_coverage.py
+++ .github/scripts/code_coverage.py
@@ -4,7 +4,7 @@ import re
 def code_coverage_report():
   table_list = get_table_list(file_paths).split(',')
 
-  html = "<details><Summary><h2>Code coverage</h2><summary></details>"
+  html = "<h2>Code coverage</h2>"
   html += "<table><tr><th>Module</th><th>Coverage</th></tr>"
   
   for index in range(0, len(table_list), 3):
