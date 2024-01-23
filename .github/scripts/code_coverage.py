5cce97e46e6b40e4be4bb94aa1de91b45937c884
bindhu-22 <94899531+bindhu-22@users.noreply.github.com>
2024-01-23 14:54:57 +0530

    Update code_coverage.py

--- .github/scripts/code_coverage.py
+++ .github/scripts/code_coverage.py
@@ -2,23 +2,40 @@ import sys
 import re
 
 def code_coverage_report():
-  table_list = get_table_list(file_paths).split(',')
+  if table_string == 'null':
+    print(get_table_list(file_paths))
+  else:
+    base_table_list = get_table_list(file_paths).split(',')
+    head_table_list = table_string.split(',')
 
-  html = "<h2>Code coverage</h2>"
-  html += "<table><tr><th>Module</th><th>Coverage</th></tr>"
-  
-  for index in range(0, len(table_list), 3):
-    start_tag = ""
-    end_tag = ""
-    link_tag = f"<a href='{table_list[index+2]}'>"
-    if index == len(table_list)-3:
-      start_tag = "<b>"
-      end_tag = "</b>"
-      link_tag = ""
-    html += f"<tr><td>{start_tag}{link_tag}{table_list[index]}{end_tag}</td>" \
-            f"<td>{start_tag}<img src={format_percentage(table_list[index+1])}>{end_tag}</td></tr>"
-  html += "</table><br><i>MIN Cverage: 35%</i>"
-  print(html)
+    html = "<h2>Code coverage</h2>"
+    html += "<table><tr><th>Module</th><th>Coverage</th>"
+    if head_table_list:
+      html += <th>Coverage</th>
+      table_list = head_table_list
+    else:
+      table_list = base_table_list
+    html += </tr>"
+    
+    for index in range(0, len(table_list), 3):
+      start_tag = ""
+      end_tag = ""
+      link_tag = f"<a href='{table_list[index+2]}'>"
+      if index == len(table_list)-3:
+        start_tag = "<b>"
+        end_tag = "</b>"
+        link_tag = ""
+      html += f"<tr><td>{start_tag}{link_tag}{table_list[index]}{end_tag}</td>" \
+              f"<td>{start_tag}<img src={format_percentage(table_list[index+1])}>{end_tag}</td>"
+      if head_table_list:
+        diff = float(head_table_list[index+1])
+        if head_table_list[index] in base_table_list:
+          moduleIndex = base_table_list.index(head_table_list[index])
+          diff -= float(base_table_list[moduleIndex+1])
+          html += f"<td>{round(diff,2)}%</td>"
+      html += "</tr>"
+    html += "</table><br><i>MIN Cverage: 35%</i>"
+    print(html)
 
 def get_table_list(file_paths):
   coveredSum=0
@@ -48,5 +65,6 @@ def format_percentage(coverage):
   return coverage
 
 file_paths = sys.argv[1].split("\n")
+table_string = sys.argv[2]
 
 code_coverage_report()
