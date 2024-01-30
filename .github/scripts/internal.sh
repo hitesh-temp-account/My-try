f8d9ee1bf3980b38edba549d6b85fd64170e87fa
bindhu-22 <94899531+bindhu-22@users.noreply.github.com>
2024-01-30 17:03:23 +0530

    Update internal.sh

--- .github/scripts/internal.sh
+++ .github/scripts/internal.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-adb shell df -h
-a=$(adb shell df -h)
-echo $a
+app_path="$(adb shell pm path com.linkedin.audiencenetwork.testapp | sed 's/package://' | sed 's/base\.apk$//')"
+
+user_data="$(adb shell "run-as com.example.breach du /data/data/com.example.breach" | awk '{$1=$1}1' | grep -w /data/data/com.example.breach | awk '{print $1}')"
+
+app_data="$(adb shell "run-as com.example.breach du $app_path" | grep -w "$app_path" | awk '{print $1}' )"
+
+total_app_usage=$(( user_data + app_data ))
+
+echo "total app usage = $(( total_app_usage/1024 )) KB"
