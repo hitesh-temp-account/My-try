329736abe89749a1d07d84c46372bfe94d15d483
bindhu-22 <94899531+bindhu-22@users.noreply.github.com>
2024-01-31 13:09:38 +0530

    Update internal.sh

--- .github/scripts/internal.sh
+++ .github/scripts/internal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-app_path="$(adb shell pm path com.linkedin.audiencenetwork.testapp | sed 's/package://' | sed 's/base\.apk$//')"
+app_path=$(adb shell pm path com.linkedin.audiencenetwork.testapp | sed 's/package://' | sed 's/base\.apk$//')
 
-user_data="$(adb shell "run-as com.example.breach du /data/data/com.example.breach" | awk '{$1=$1}1' | grep -w /data/data/com.example.breach | awk '{print $1}')"
+user_data=$(adb shell "run-as com.example.breach du /data/data/com.example.breach" | awk '{$1=$1}1' | grep -w /data/data/com.example.breach | awk '{print $1}')
 
-app_data="$(adb shell "run-as com.example.breach du $app_path" | grep -w "$app_path" | awk '{print $1}' )"
+app_data=$(adb shell "run-as com.example.breach du $app_path" | grep -w "$app_path" | awk '{print $1}')
 
 echo $user_data
 echo $app_data
