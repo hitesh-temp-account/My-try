e20c0849db3f8776d50803c8f06948065955654f
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2024-02-05 18:48:30 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/breach/MainActivity.java
+++ app/src/main/java/com/example/breach/MainActivity.java
@@ -1,5 +1,5 @@
 // Copyright 2019 Foo. All rights reserved. MIT1.
-// Copyright 2019 Foo. All rights Reserved. MIT.
+// Copyright 2019 Foo. All rights reserved. MIT. 234234234
 package com.example.breach;
 
 import androidx.appcompat.app.AppCompatActivity;
