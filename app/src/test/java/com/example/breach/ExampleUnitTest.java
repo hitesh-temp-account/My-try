06d9d558c9f6bfc21dbdbced229f9ab930e23634
hiteshkuberr <152952135+hiteshkuberr@users.noreply.github.com>
2024-02-26 19:19:29 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/breach/ExampleUnitTest.java
+++ app/src/test/java/com/example/breach/ExampleUnitTest.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) test Corporation.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package com.example.breach;
 
 import org.junit.Test;
